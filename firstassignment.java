060f5a8530027ad94f03862ec4b4ddb3b69f3b94
kdb1418 <31638937+kdb1418@users.noreply.github.com>
2017-09-05 14:48:50 +0900

    Add files via upload

---
+++ firstassignment.java
@@ -0,0 +1,12 @@
+
+public class firstassignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("You student id number: 2014301011");
+		System.out.println("Your name: Kim Dabin");
+		System.out.println("Your mobile phone number: 010-2792-1984");
+		System.out.println("Your GitHub address: kdb1418");
+	}
+
+}
